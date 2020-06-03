cfc1ad03c4c670952863e957a7f71afff9f4dd04
Ilya Chagovtsev <ichagovtsew@gmail.com>
2020-06-03 12:40:53 +0200

    fix input and slider

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,17 @@
 "use strict";
 $(document).ready(function() {
-    $('.slider').slick();
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        draggable: true,
+        swipe: true,
+        touchTreshold: 10,
+        centerMode: false
+    });
 });
