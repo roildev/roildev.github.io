32eb434744e78c614f386fd4c94c67c804b21ba0
Ilya Chagovtsev <ichagovtsew@gmail.com>
2020-06-08 12:37:20 +0200

    circle-big, circle-small

--- src/script/main.js
+++ src/script/main.js
@@ -15,3 +15,26 @@ $(document).ready(function() {
         centerMode: false
     });
 });
+
+const servisContent = document.querySelectorAll('.servis__content'),
+    circleBig = document.querySelectorAll('.circle-big'),
+    circleSmall = document.querySelectorAll('.circle-small')
+
+console.log(servisContent);
+
+servisContent.forEach(card => {
+    card.addEventListener('mouseover', (event) => {
+        if (event.target.closest('.servis__content')) {
+            card.childNodes[1].classList.remove('circle-big-active');
+            card.childNodes[1].classList.add('circle-big-active');
+            card.childNodes[3].classList.remove('circle-small-active');
+            card.childNodes[3].classList.add('circle-small-active');
+        }
+    });
+    card.addEventListener('mouseleave', (event) => {
+        if (event.target.closest('.servis__content')) {
+            card.childNodes[1].classList.remove('circle-big-active');
+            card.childNodes[3].classList.remove('circle-small-active');
+        }
+    });
+});
