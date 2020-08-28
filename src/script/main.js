2f532cc2bfa9f7937bc12d987dfa529cad7cf910
Roman Chagovtsev <4agovtsev@gmail.com>
2020-08-28 11:16:49 +0200

    popup js

--- src/script/main.js
+++ src/script/main.js
@@ -34,8 +34,9 @@ $(document).ready(function() {
         totalPrice = document.getElementById('total-price'),
         formErrorMessage = document.querySelectorAll('.form-error'),
         calculation = document.getElementById('calc'),
-        submitFormCalc = document.getElementById('submit-form-calc'),
-        allForms = document.querySelectorAll('form');
+        btnsOpenPopup = document.querySelectorAll('.open-popup'),
+        allForms = document.querySelectorAll('form'),
+        popups = document.querySelectorAll('.popup');
 
                 
     let arrowShow = false,
@@ -150,6 +151,29 @@ $(document).ready(function() {
         }
         console.log(formData)
     };
+
+    const openPopup = (name) => {
+        popups.forEach(popup => {
+            if (popup.attributes.title.nodeValue === name) {
+                const popupContent = popup.querySelector('.popup__content');
+
+                popup.style.visibility = 'visible';
+                popupContent.classList.add('popup__content-active');
+
+                popup.addEventListener('click', (event) => {
+                    if (!event.target.closest('.popup__content') ||
+                        event.target.closest('.popup__close')) {
+                            closePopup(popupContent, popup);
+                    }
+                });
+            }
+        });
+    }
+
+    const closePopup = (popupContent, popup) => {
+        popupContent.classList.remove('popup__content-active');
+        popup.style.visibility = 'hidden';
+    }
 
 
     // Listeners
@@ -337,6 +361,13 @@ $(document).ready(function() {
             }
         });
     })
+
+    btnsOpenPopup.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            openPopup(event.target.name);
+        });
+    });
+    
 
 });
 
