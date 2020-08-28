76cdc907f35f2f51691cfff9f0b65ff1dd1e7358
Roman Chagovtsev <4agovtsev@gmail.com>
2020-08-28 15:36:58 +0200

    fetch start

--- src/script/main.js
+++ src/script/main.js
@@ -37,6 +37,15 @@ $(document).ready(function() {
         btnsOpenPopup = document.querySelectorAll('.open-popup'),
         allForms = document.querySelectorAll('form'),
         popups = document.querySelectorAll('.popup');
+    
+    // send form
+    const errorMessage = 'Что-то пошло не так...',
+        loadMessage = 'Загрузка...',
+        successMessage = 'Спасибо мы скоро с Вами свяжемся!',
+        statusMessage = document.createElement('div');
+
+    statusMessage.style.cssText = 'font-size: 2rem';
+    statusMessage.style.color = '#19b5fe';;
 
                 
     let arrowShow = false,
@@ -85,11 +94,11 @@ $(document).ready(function() {
     }
 
     
-    const errorMessage = (message, target) => {
-        target = message === 'show' ?
-        target.nextElementSibling.style.display = 'block' :
-        target.nextElementSibling.style.display = 'none';
-    }
+    // const errorMessage = (message, target) => {
+    //     target = message === 'show' ?
+    //     target.nextElementSibling.style.display = 'block' :
+    //     target.nextElementSibling.style.display = 'none';
+    // }
 
     const addElement = (nameBlock, amount) => {
         if (nameBlock === 'age') {
@@ -134,6 +143,15 @@ $(document).ready(function() {
         }
     }
 
+    const postData = (formData) => fetch('../mail-send.php', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+
+    
     const formValidation = (form) => {
         let formData = {};
         formData.formName = form.name
@@ -354,11 +372,50 @@ $(document).ready(function() {
     });
 
     allForms.forEach(form => {
-        form.addEventListener('click', (event) => {
-            if (event.target.type ==='submit') {
-                console.log(form.value);
-                formValidation(form);
-            }
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(form);
+            let body = {};
+    
+            form.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+    
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+            // console.log(body)
+    
+            postData(body)
+                .then(response => {
+                    if (response.status !== 200) {
+                        throw new Error('status network not 200');
+                    }
+                    statusMessage.textContent = successMessage;
+                    setTimeout(() => {
+                        statusMessage.textContent = '';
+                    }, 5000);
+    
+                    for (let i = 0; i < allInputs.length; i++) {
+                        allInputs[i].value = '';
+                    }
+    
+                    form.reset();
+                })
+                .catch(error => {
+                    statusMessage.textContent = errorMessage;
+                    setTimeout(() => {
+                        statusMessage.textContent = '';
+                    }, 5000);
+                    console.error(error);
+                });
+            // console.log('body: ', body);
+
+            // console.log(form)
+            // if (event.target.type ==='submit') {
+            //     console.log(form.value);
+            //     formValidation(form);
+            // }
         });
     })
 
